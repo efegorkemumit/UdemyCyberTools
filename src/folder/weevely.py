82bebf1714922146f989e190eb1b81629dfb6eee
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-06-27 10:09:14 +0300

    Udemy Course Python  Etical Tools
    Udemy Course Python  Etical Tools

---
+++ src/folder/weevely.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+
+
+import os
+
+
+def figlet():
+	os.system("setxkbmap tr")
+	os.system("apt-get install figlet")
+	os.system("clear")
+	os.system("figlet efe gorkem umit")
+	
+def weevely_hello():
+	print(""" 
+	
+	-------------------------------------------
+			WEEVELY
+	-------------------------------------------
+	
+	
+	
+	
+	""")
+	os.system("weevely" )
